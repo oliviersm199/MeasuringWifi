031eca9b2de3f1477af2bab9e296b9474dad0a7d
OlivierSM <olivier.morissette@gmail.com>
2016-10-17 11:33:05 -0400

    Added example csv and json files and requirements file

--- installation.sh
+++ installation.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport
+pip install netifaces
--- measurement.py
+++ measurement.py
@@ -7,6 +7,7 @@ This script will take readings of your current location and store the answers to
 
 The scripts use the following two tools to determine this information:
   - Airport: Command Line wireless utility for Mac OSX.
+  - Netifaces: Python Library for working with interfaces
 
 Because of the use of the airport command the program only supports being run
 on Mac OSX systems.
@@ -23,6 +24,8 @@ import datetime
 import sys
 import json
 import time
+import netifaces
+import re
 
 # Number of times you attempt using airport tool
 number_attempts = 5
@@ -42,6 +45,7 @@ def _get_access_points(dateandtime,location):
         - HT
         - CC
         - Security (WPA2 generally)
+
     '''
     try:
         result = check_output(["airport","-s"],shell=False)
@@ -68,6 +72,13 @@ def _get_access_points(dateandtime,location):
     return True
 
 def get_access_points(dateandtime,location):
+    '''
+    
+    The airport tool fails if used multiple times in a short period of time. One way around this is 
+    to try using multiple times. An attempt is made once every second. If the attempt doesn't work after
+    number of attempts time, the user will be prompted to try again.
+
+    '''
     success = False
     for i in range(number_attempts):
         success = _get_access_points(dateandtime,location)
@@ -76,28 +87,43 @@ def get_access_points(dateandtime,location):
         time.sleep(1)
     return success
 
+def get_users_connected():
+    '''
+    
+    This method will determine the number of devices that are currently connected to a network using ping and arp.
+    First the program will automatically detect all of your interfaces and check for interfaces on MAC osx for WIFI (en0 and en1).
+    Then it will attempt to get the network address and then send a broadcast ping to all devices. This should update the local ARP
+    table which we then query to get all of the devices on the network.
+
+    '''
+    broadcast_addresses = []
+    for interface in netifaces.interfaces():
+        if interface.startswith('en') and netifaces.AF_INET in netifaces.ifaddresses(interface):
+            links = netifaces.ifaddresses(interface)[netifaces.AF_INET]
+            try:
+                for item in links:
+                    broadcast_addresses.append(item['broadcast'])
+            except IndexError as e:
+                print("No broadcast found on this interface")
+
+
+    users_connected = []
+    for address in broadcast_addresses:
+        print("Pinging: %s"%(address))
+        res = check_output(['ping', '-c', '5', address])
+        objects_in_network = check_output(['arp','-a'])
+        answer = [user for user in objects_in_network.strip().split('\n') if re.match(r'\web[0-9]+.wireless.mcgill.ca[\w\W]',user.strip())]
+        users_connected.append({address:len(answer)})
+
+    return users_connected
+
+
 
 def get_current_connection_info(dateandtime, location):
     '''
 
-    Will use the airport commandline tool to get info about the user's current connection.
+    Will use the airport commandline tool to get info about the user's current connection. See examples for information recorded.
 
-    Information provided includes:
-        - agrCtlRSSI
-        - agrExtRSSI
-        - agrCtlNoise
-        - agrExtNoise
-        - state
-        - op mode
-        - lastTxRate
-        - maxRate
-        - lastAssocStatus
-        - 802.11 auth
-        - link auth
-        - BSSID
-        - ssid
-        - MCS
-        - Channel
     '''
     try:
         result = check_output(["airport","-I"],stderr=STDOUT)
@@ -105,10 +131,10 @@ def get_current_connection_info(dateandtime, location):
         return False
 
     result = dict((elem.strip().split(":",1) for elem in result.strip().split('\n')))
+    result['users_connected'] = get_users_connected()
     name = "Connection Info: " + str(dateandtime) + ' - ' + str(location) + ".json"
     with open(name, 'w') as outfile:
         json.dump(result, outfile,indent=4, sort_keys=True)
-
 
 
 if __name__ == '__main__':
@@ -122,6 +148,7 @@ if __name__ == '__main__':
 
     # Step 1: Getting Access points. Attempt to use airport tool multiple times. If it doesn't work then
     # we prompt the user to restart
+    print("Getting Access Point Data")
     step_1_completed = False
     counter = 1
     while not step_1_completed:
@@ -136,6 +163,8 @@ if __name__ == '__main__':
                 sys.exit(-1)
 
     # Step 2: Getting Information about current connection and storing in JSON File
+    print("Getting Current Connection Info")
     get_current_connection_info(dateandtime,location)
+
 
     print("Done")
