3488691e4b6b8398f886118bf4af85dbbc67c12c
Can Rozanes <canrozanes@Can-Rozaness-MacBook-Pro.local>
2016-10-24 17:13:23 -0400

    added pip instlalation

--- installation.sh
+++ installation.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo ln -s /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport /usr/local/bin/airport
+sudo easy_install pip
 pip install netifaces
